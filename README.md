b11945061cc129832d467ce07c189759bda2042d
Naheel-Azawy <naheelazawy@gmail.com>
2018-02-18 02:03:08 +0300

    app menu

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Overview Titles Shrink (GNOME Extension)
-Shrink the titles in the overview to fit the window size
+Shrink the titles in the overview to fit the window size. Also does the same for the apps menu.
 
 ## Before
 ![screenshot](screenshot1.png)
+![screenshot](screenshot3.png)
 ## After
 ![screenshot](screenshot2.png)
+![screenshot](screenshot4.png)
--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 const Workspace = imports.ui.workspace;
+const AppDisplay = imports.ui.appDisplay;
+const PopupMenu = imports.ui.popupMenu;
 
 let origins = [];
 
@@ -31,15 +33,23 @@ function init() {
 
 function enable() {
 	inject_fun_top(Workspace.WindowOverlay.prototype, "relayout", function(animate) {
-        let [cloneX, cloneY, cloneWidth, cloneHeight] = this._windowClone.slot;
-        let borderWidth = cloneWidth + 2 * this.borderSize;
+		let [cloneX, cloneY, cloneWidth, cloneHeight] = this._windowClone.slot;
+		let borderWidth = cloneWidth + 2 * this.borderSize;
 		if (this.title.width >= borderWidth)
-        	this.title.width = borderWidth;
+			this.title.width = borderWidth;
+	});
+	inject_fun_top(AppDisplay.AppIconMenu.prototype, "_appendMenuItem",  function(labelText) {
+		if (labelText.length > 50)
+			labelText = labelText.substring(0, 50) + "...";
+		let item = new PopupMenu.PopupMenuItem(labelText);
+		this.addMenuItem(item);
+		return item;
 	});
 }
 
 function disable() {
 	remove_fun(Workspace.WindowOverlay.prototype, "relayout");
+	remove_fun(AppDisplay.AppIconMenu.prototype, "_appendMenuItem");
 }
 
 
